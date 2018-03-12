2ae2e8b87f100558281cd76b93fb06fbf88d1cf5
zykhoh <zykhoh@gmail.com>
2018-03-12 17:06:08 +0800

    feature added title description and date fields

--- src/main/resources/static/app/core/video/video.service.js
+++ src/main/resources/static/app/core/video/video.service.js
@@ -8,8 +8,7 @@ factory('Video',
         return $resource('', {}, {
             query   : {
                 method:'GET',
-                url: 'http://192.168.1.102:8080/video/all',
-                isArray: true
+                url: 'http://localhost:8080/api/videos?sort=id,desc'
             },
             get : {
                 method:'GET',
